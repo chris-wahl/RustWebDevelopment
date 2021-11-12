5a4f03a0140cc99004b5c07b960aad6bd6df3277
Christopher Wahl <christopher@nothingmuch.net>
2021-11-12 18:12:54 -0500

    Chp 2: Simplify traits

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::str::FromStr;
 use crate::types::{Question, QuestionId};
 
 mod types;
@@ -5,10 +6,10 @@ mod types;
 
 fn main() {
     let question = Question::new(
-        QuestionId("1".to_string()),
+        QuestionId::from_str("1").expect("No id provided"),
         "First Question".to_string(),
         "Content of question".to_string(),
         Some(Vec::from(["faq".to_string()]))
     );
-    println!["{}", question];
+    println!["{:?}", question];
 }
--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,36 +1,35 @@
-use std::fmt::{Debug, Display, Error, Formatter};
+use std::io::{ErrorKind};
+use std::str::FromStr;
 
+#[derive(Debug)]
 pub struct Question {
     id: QuestionId,
     title: String,
     content: String,
-    tags: Option<Vec<String>>
+    tags: Option<Vec<String>>,
 }
 
-pub struct QuestionId(pub String);
+#[derive(Debug)]
+pub struct QuestionId(String);
 
 impl Question {
     pub fn new(id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) -> Self {
         Question {
-            id, title, content, tags
+            id,
+            title,
+            content,
+            tags,
         }
     }
 }
 
-impl Display for Question {
-    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
-        write![f, "{}, title: {}, content: {}, tags: {:?}", self.id, self.title, self.content, self.tags]
-    }
-}
+impl FromStr for QuestionId {
+    type Err = std::io::Error;
 
-impl Display for QuestionId {
-    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
-        write![f, "id: {}", self.0]
-    }
-}
-
-impl Debug for Question {
-    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
-        write![f, "{:?}", self.tags]
+    fn from_str(id: &str) -> Result<Self, Self::Err> {
+        match id.is_empty() {
+            false => Ok(QuestionId(id.to_string())),
+            true => Err(std::io::Error::new(ErrorKind::InvalidInput, "No id provided"))
+        }
     }
 }
